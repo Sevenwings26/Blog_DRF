675f5ec8c5c7851684eb59b970cc4839a3280831
Sevenwings26 <iarowosola@yahoo.com>
2024-07-02 18:53:59 +0100

    Update

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from . models import Post, Category
 
 # Create your tests here.
+class Test_CreatePost(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_category = Category.objects.create(name = "django")
+        test_user = User.objects.create_user(username = "testuser", password = "test123")
